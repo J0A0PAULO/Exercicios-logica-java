6ed335b6820171a7052f48c3944fb251889f1076
João <moreirajoaopaulo951@gmail.com>
2026-06-01 10:26:10 -0300

    exercicio soma

--- soma/Soma.java
+++ soma/Soma.java
@@ -1,3 +1,20 @@
-public class Soma {
+import java.util.Scanner;
 
+public class Soma {
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("digite numero 1");
+        int n1  = sc.nextInt();
+
+        System.out.println("digite numero 2");
+        int n2  = sc.nextInt();
+
+        int soma = n1 + n2;
+
+        System.out.println("resuldado da soma");
+        System.out.println(soma);
+    }
 }
