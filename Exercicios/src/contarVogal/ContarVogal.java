70d08173fb4e992c27b90a2834f4f88f7236c299
João <moreirajoaopaulo951@gmail.com>
2026-06-05 10:27:22 -0300

    exercicio ContarVogal

---
+++ Exercicios/src/contarVogal/ContarVogal.java
@@ -0,0 +1,21 @@
+package contarVogal;
+
+public class ContarVogal  {
+   public static void main(String[] args) throws Exception {
+        String str = "leetcode";
+        String [] letras = str.split("");
+        int contador =0;
+
+
+
+        for(int i = 0; i < letras.length; i++ )  {
+
+            if (letras[i].equals("a") || letras[i].equals("e") ||letras[i].equals("i") || letras[i].equals("o") || letras[i].equals("u")|| letras[i].equals("aõ"))  {
+                contador++;
+            }
+
+        }
+        System.out.println(contador);
+    }
+
+}
--- Exercicios/src/inverterArray/inverteArray.java
+++ Exercicios/src/inverterArray/inverteArray.java
@@ -7,7 +7,7 @@ public class inverteArray {
 
         for (int i = nums.length - 1; i >= 0; i--) {
             System.out.println(nums[i]);
-            break;
+
         }
     }
 }
