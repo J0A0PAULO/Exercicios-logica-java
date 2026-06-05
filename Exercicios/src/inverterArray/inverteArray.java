f1c894cc813ad03507f37ed08f266a19ede9c152
João <moreirajoaopaulo951@gmail.com>
2026-06-05 09:49:44 -0300

    exercicio inverteArray

---
+++ Exercicios/src/inverterArray/inverteArray.java
@@ -0,0 +1,13 @@
+package inverterArray;
+
+public class inverteArray {
+    static void main(String[] args) {
+
+        int [] nums = {1,2,3,4,5};
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            System.out.println(nums[i]);
+            break;
+        }
+    }
+}
